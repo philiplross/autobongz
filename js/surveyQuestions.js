6875adac5c2f0443ac3dcff4a7b8e01a90c06d48
philiplross <philiplross@gmail.com>
2019-01-11 11:55:28 -0800

    survey started in js

---
+++ js/surveyQuestions.js
@@ -0,0 +1,20 @@
+var questionOne = "do you prefer a straight straight shot bootom or beaker?";
+
+var questionTwo = "choose which percs you like from best to worst";
+
+var questionThree = "Do you perfer smoking flower or concentrates";
+
+var questionFour = "which do you want your collection or peice in, 18mm or 14mm>";
+
+var questionFive = "Do you perfer natural downstem or angled diffused downstem?";
+
+console.log(questionOne);
+alert(questionOne);
+console.log(questionTwo);
+alert(questionTwo);
+console.log(questionThree);
+alert(questionThree)
+console.log(questionFour);
+alert(questionFour);
+console.log(questionFive);
+alert(questionFive);
